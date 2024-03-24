f315d44f4b41bee671bc5ecd5f87272cd1487cf1
Viandra Stefani <piyanstefani@gmail.com>
2024-03-24 08:14:13 +0700

    Perbaikan

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import set
 import requests as re
 from bs4 import BeautifulSoup
 
-def main(inputed=input("Masukkan Link: "), jumlah=2):
+def main(inputed=input("Masukkan Link: "), jumlah=int(input("Masukkan Jumlah Halaman"))):
     set.halaman1(porns=inputed)
-    for i in range(2, jumlah):
+    for i in range(2, jumlah+1):
         linkstr = str(f"{inputed}?page={i}")
-        set.halaman1(inputed + linkstr)
-        print(inputed + linkstr)
+        set.halaman1(linkstr)
+        print(linkstr)
 
-main(inputed="https://www.pornhub.com/pornstar/eliza-ibarra/videos", jumlah=13)
+main()
